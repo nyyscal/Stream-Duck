a772a0aee57a3424445b12c9177d685337558b48
Nischal <nyyscal541556@gmail.com>
2025-05-14 11:43:19 +0545

    Better Auth + Google Auth Setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,17 @@
 import Header from '@/components/Header'
 import VideoCard from '@/components/VideoCard'
+import { dummyCards } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
     <main className='wrapper page'> 
     <Header title="All Videos" subHeader="Public Library"/>
-      <VideoCard 
-      id="1"
-      title="SnapChat Message"
-      thumbnail="/assets/samples/thumbnail (1).png"
-      createdAt={new Date("2025-05-01")}
-      userImg="/assets/images/alex.png"
-      views={10}
-      visibility="public"
-      duration={156}
-      username='Alex'
-      />
+      <section className='video-grid'>
+     {dummyCards.map((card)=>(
+      <VideoCard key={card.id} {...card} />
+     )) }
+      </section>
     </main>
   )
 }
--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,12 +1,18 @@
 import Header from '@/components/Header'
+import VideoCard from '@/components/VideoCard'
+import { dummyCards } from '@/constants'
 import React from 'react'
 
 const Profile = async({params}:ParamsWithSearch) => {
-  const {id} = await params
+  // const {id} = await params
   return (
     <div className='wrapper page'>
       <Header subHeader='nyyscal541556@gmail.com' title='Nischal Malla' userImg='/assets/images/dummy.jpg'/>
-     <h1 className='text-2xl font-karla'>USER ID:{id}</h1> 
+      <section className='video-grid'>
+     {dummyCards.map((card)=>(
+      <VideoCard key={card.id} {...card} />
+     )) } 
+      </section>
      </div>
   )
 }
